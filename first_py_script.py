350cd6ca9e9da32c8758f57b591bc7df55c6e64f
Alan <alanohcz94@gmail.com>
2024-06-20 19:41:04 +0800

    Add a line more of nonsense to be commented out and removed import os

--- first_py_script.py
+++ first_py_script.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 
 directory = sys.argv[1]
 
 with open(directory, 'r') as f:
     print(f.readlines())
+
+
+Added more nonsese line here
 
 Adding nonsense that shouldn't be here. This should be used as a comment.
 
