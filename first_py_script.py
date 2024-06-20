d1e1e8fadc64fba05621147ed2b043d83daf9511
Alan <alanohcz94@gmail.com>
2024-06-20 20:56:03 +0800

    Updated code, and added a print statement to test merge conflict and understand what it is

--- first_py_script.py
+++ first_py_script.py
@@ -7,7 +7,7 @@ directory = sys.argv[1]
 with open(directory, 'r') as f:
     print(f.readlines())
 
-
+print("Hello there I hope I have created a conflict to merge")
 #Added more nonsese line here
 #Adding nonsense that shouldn't be here. This should be used as a comment.
 
